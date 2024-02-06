0e07c84f95a205d69c911cfc379efbc683bbb20f
Nana Adjei Manu <claeusdev@gmail.com>
2024-02-06 23:01:18 +0000

    chore: add error handling

--- app/forms.py
+++ app/forms.py
@@ -24,7 +24,7 @@ class RegistrationForm(FlaskForm):
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user is not None:
-            raise ValidationError("Please use a different username")
+            raise ValidationError("Username already taken.")
 
 
     def validate_email(self, email):
@@ -36,3 +36,13 @@ class EditProfileForm(FlaskForm):
     username = StringField("username", validators=[DataRequired()])
     about_me = TextAreaField("About me", validators=[Length(min=0,max=256)])
     submit = SubmitField("Update profile")
+
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=username.data).first()
+            if user is not None:
+                raise ValidationError("Username already taken")
